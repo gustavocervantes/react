a31604fd35b18bd0e585673cc734a0ad0313e7a0
Gustavo Cervantes <gustavocervantes992@gmail.com>
2022-06-30 19:26:49 -0300

    enviadn

---
+++ index.js
@@ -0,0 +1,80 @@
+console.log('Fede node')
+// var nombre = 'juan'
+// console.log('Hola mundo');
+// const obj = {
+//     foo: function(){
+//         console.log('foo')
+//     },
+//     bar: function () {
+//         //var that = this;
+//         document.addEventListener('click', (e)=>{
+//           foo()
+//       })
+//     }
+// }
+
+// console.log(obj)
+
+// obj.bar()
+
+// class Animal { 
+//     constructor(sonido = 'ola-k-ac'){
+//         this.sonidoPrimitivo = sonido
+//     }
+
+//     emitirSonido(){
+//         console.log(this.sonidoPrimitivo)
+//     }
+    
+// }
+
+//let perro = new Animal
+// console.log(perro.emitirSonido())
+
+
+
+
+// class Persona extends Animal{
+//     constructor(altura){
+//         super('hola soy una persona')
+//         this.altura= 1.20
+
+//     }
+
+//     hablar(){
+//         return this.emitirSonido()
+//     }
+
+//     getAltura(){
+//         return this.altura
+//     }
+
+//     correr(){
+//         console.log('vamos a correr');
+//     }
+// }
+
+// var persona = new Persona()
+// console.log(persona);
+
+// var nombre= 'dan'
+// let apellido = 'abramov'
+// const cuenta = 'Fede'
+
+// // console.log("mi nombre es: "+ nombre +' el apellido es: \n'+ apellido );
+
+// console.log( ` hola ${nombre} ${apellido} saludos a ${cuenta} ` );
+
+
+
+// function saludar(nombre = 'fede'){
+//     console.log( nombre )
+// }
+
+
+// saludar(null)
+
+
+
+
+
